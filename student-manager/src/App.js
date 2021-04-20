a2a6b765d5f5e8052bf977543a7b72bb63fd0254
jimblu <jimmy.pezp@gmail.com>
2021-04-21 00:36:04 +0200

    Update App.js

--- student-manager/src/App.js
+++ student-manager/src/App.js
@@ -22,7 +22,7 @@ function App() {
 
             <Navbar.Brand>
               <Link to={"/create-student"} className="nav-link">
-                React MERN Stack App
+                Student Manager
               </Link>
             </Navbar.Brand>
 
